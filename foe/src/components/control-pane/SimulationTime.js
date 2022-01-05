04ef0967147668be48dfcade470a4799adb17368
Danny Flynn <p.daniel.flynn@gmail.com>
2022-01-05 17:15:01 -0500

    add gui time controls

--- foe/src/components/control-pane/SimulationTime.js
+++ foe/src/components/control-pane/SimulationTime.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 const SimulationTime = ({ updateTime }) => {
-    const [simTimeMult, setTimeMult] = useState('');
+    const [simTimeMult, setTimeMult] = useState('1000');
 
     const onSubmit = (e) => {
         e.preventDefault()
 
         updateTime({ simTimeMult });
-        setTimeMult('')
+        setTimeMult(simTimeMult)
     }
 
 
     return (
         <form className='simTimeForm' onSubmit={onSubmit}>
             <div className='orbit'>
-                <label>Simulation Time</label>
-                <input type='text' placeholder='e.g. 1000' value={simTimeMult} 
-                onChange={(e) => setTimeMult(e.target.value)}
-                style={{width: '25%', position: 'relative', left: '15%',
-                        margin: '0px', padding: '0px'}}
-                />
+                <label>Simulation Time (sec/sec)</label>
+                <div>
+                    <input type='submit' value="<<" onClick={() => setTimeMult(simTimeMult / 10)}/>
+                    <input type='submit' value="<" onClick={() => setTimeMult(simTimeMult / 2)}/>
+                    <input type='text' placeholder='e.g. 100' value={simTimeMult} 
+                    onChange={(e) => setTimeMult(e.target.value)}
+                    style={{width: '25%',
+                            margin: '0px', padding: '0px'}}
+                    />
+                    <input type='submit' value=">" onClick={() => setTimeMult(simTimeMult * 2)}/>
+                    <input type='submit' value=">>" onClick={() => setTimeMult(simTimeMult * 10)}/>
+                    <input type='submit' value="Pause" onClick={() => setTimeMult(0)}/>
+                </div>
             </div>
         </form>
     )
